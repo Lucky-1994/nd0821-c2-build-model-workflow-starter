a41a275b7a196b5b26729eec5d74907d1d73651d
Lucky-1994 <104618180+Lucky-1994@users.noreply.github.com>
2022-05-06 10:52:18 +0200

    fix(basic-cleaning): filter by geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -27,6 +27,10 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # filter by geolocation
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Save as csv
     file_name = "clean_sample.csv"
     df.to_csv(file_name, index=False)
